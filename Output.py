5accc94fb012f44a96443252bb29a3d431660524
bjohnalok <johnalok30323@gmail.com>
2024-12-21 23:59:29 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,7 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+>>> 
+==== RESTART: C:/Users/jonol/OneDrive/Desktop/student performace analzyer.py ===
+Student 01: Total Marks - 263, Percentage - 87.66666666666667%, Rank - 2
+Student 02: Total Marks - 276, Percentage - 92.0%, Rank - 1
